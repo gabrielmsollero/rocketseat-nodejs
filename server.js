2f827c04c8761d07e34cd8e3a27e2a24f67ddac4
Gabriel <sollero.gms@gmail.com>
2024-02-28 10:14:19 -0300

    feat: create initial setup of fastify server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-import { createServer } from "node:http";
+import fastify from "fastify";
 
-const server = createServer((request, response) => {
-  response.write('hello world!')
-  return response.end()
+const server = fastify();
+
+server.get('/', () => {
+  return "Hello world!"
+})
+
+server.listen({
+  port: 3333
 });
-
-server.listen(3333);
