935e57ad0a9fa66cb2ce83b7ae52a4845f49d42b
Gabriel <sollero.gms@gmail.com>
2024-02-28 10:02:13 -0300

    feat: create example server without any frameworks

---
+++ server.js
@@ -0,0 +1,8 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  response.write('hello world!')
+  return response.end()
+});
+
+server.listen(3333);
